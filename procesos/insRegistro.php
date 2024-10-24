3042658f376188c74cf9461d7d103e282592be1a
PC-ERIC <eric.alcazarc@gmail.com>
2024-10-24 19:24:11 +0200

    Registro dle usuario con alerta funcionando

--- procesos/insRegistro.php
+++ procesos/insRegistro.php
@@ -1,11 +1,9 @@
-
 <?php
-
 // Inicia la sesión
 session_start();
 
 // Incluye el archivo de conexión a la base de datos
-include './conexion.php';
+include '../conexion.php';
 
 // Verifica si el formulario ha sido enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -21,24 +19,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $check_email = "SELECT * FROM user WHERE correo_user='$correo_user'";
     $result = mysqli_query($conn, $check_email);
 
-    // Si en el select hay más de 0 consultas significa que hay un usuario que ya está creado con ese mail, si el valor es 0 onull significa que no
+    // Verifica si se encontraron registros
     if (mysqli_num_rows($result) > 0) {
-        echo "<script>alert('El correo ya está registrado. Intenta con otro.');</script>";
+        echo "<script>alert('El correo ya está registrado. Intenta con otro.'); window.location.href = '../index.php';</script>";
     } else {
-
         // Inserta el nuevo usuario en la base de datos
         $sql = "INSERT INTO user (nombre_user, contrasena, correo_user) VALUES ('$nombre_user', '$hashed_password', '$correo_user')";
         
         if (mysqli_query($conn, $sql)) {
-            echo "<script>alert('USUARIO REGISTRADO.');</script>";
-            header("Location: index.php"); 
-            exit();
+            echo "<script>alert('USUARIO REGISTRADO.'); window.location.href = '../index.php';</script>";
         } else {
             echo "Error: " . mysqli_error($conn);
         }
     }
 }
-
 
 mysqli_close($conn);
 ?>
